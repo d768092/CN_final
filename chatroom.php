da648bb6d9b3f91287e4817fccceaac06d030a4c
b06902067 <b06902067@linux2.csie.ntu.edu.tw>
2020-01-16 15:48:20 +0800

    get msg

--- chatroom.php
+++ chatroom.php
@@ -60,7 +60,7 @@ else{
 <div class="sidebar">
 <input id="findname" type="text" placeholder="搜尋使用者名稱">
 <button id="finduser">搜尋</button>
-<div id="error" style="color:red;"></div>
+<div id="error" style="color:red; overflow:hidden"></div>
 <div id="chatlist">
 <?php
 $namehash = hash('sha256', $username);
@@ -77,13 +77,17 @@ foreach($data as $key => $value){
 </div>
 <div class="middle">
 <div class="chat_to" id="chat_to">選個朋友來聊天吧!</div>
-<div class="msg" id="msg"></div>
+<div class="msg" id="msg">
+<!--<div class="others">別人的訊息框</div>-->
+<!--<div class="mine">我的訊息框</div>-->
+</div>
+<div class="msg_end" id="msg_end" style="height:0px;"></div>
 <input class="input_msg" id="input_msg" type="text" placeholder="輸入訊息">
 <button type="button "id="commit" onclick="sendmsg()" style="float: right;">傳送</button> 
 <form action = "uploadfile.php" method = "POST" enctype = "multipart/form-data" target = "_blank" id = "upload" onsubmit = "sendmsg_file()">
 	<input type = "file" name = "image" onclick="setvalue()" onchange="showname()"/>
 	<input type = "submit"/>
-	<input type = "hidden" name = "chat_to"/>
+	<!--<input type = "hidden" name = "chat_to"/>-->
 </form>
 <form action="download.php" method = "POST" enctype="multipart/form-data" targer="_blank" id="download">
 	<input type = "text" name="path"/>
@@ -111,14 +115,24 @@ foreach($data as $key => $value){
 		}
 		function sendmsg()
 		{
-			//$('#msg').text("HEY");
 			$.post('sendmsg.php',
 			{chat_to: document.getElementById("chat_to").textContent,
 			 input_msg: document.getElementById("input_msg").value},
 			 function(response){
+			 	 $('#input_msg').val('');
 				 $('#msg').html(response);
+				 msg.scrollTop=msg.scrollHeight;
 			 })
 		}
+		function getmsg(){
+			$.post('sendmsg.php',
+				{chat_to: document.getElementById("chat_to").textContent},
+				function(response){
+					$('#msg').html(response);
+				}
+			)
+		}
+		setInterval(getmsg, 5000);
 </script>
 </body>
 </html>
--- sendmsg.php
+++ sendmsg.php
@@ -12,6 +12,7 @@ $chat_to = $_POST['chat_to'];
 $input_msg = $_POST['input_msg'];
 $namehash = hash('sha256', $username);
 $jsonfile_user = 'user_data/'.substr($namehash, 0, 16).'.json';
+if(!is_file($jsonfile_user)) exit('');
 $json_string_user = file_get_contents($jsonfile_user);
 $data_user = json_decode($json_string_user, true);
 $msgfile = $data_user[$chat_to];
@@ -21,8 +22,11 @@ if(is_file($msgfile)){
 	$json_string_msg = file_get_contents($msgfile);
 	$data_msg = json_decode($json_string_msg, true);
 	$user_time = $username."_".$time;
-	if(isset($input_msg)&&$input_msg!='') $data_msg[$user_time] = $input_msg;
-
+	if(isset($input_msg)&&$input_msg!=''){
+		$data_msg[$user_time] = $input_msg;
+		$json_string_msg = json_encode($data_msg);
+		file_put_contents($msgfile, $json_string_msg);
+	}
 	foreach($data_msg as $key => $value)
 	{
 		if(strpos($key, 'upload') !== false){ 
@@ -31,8 +35,6 @@ if(is_file($msgfile)){
 			echo nl2br("User ".substr($key,0,-11)." says ".$value."\n");
 		}
 	}
-	$json_string_msg = json_encode($data_msg);
-	file_put_contents($msgfile, $json_string_msg);
 }
 ?>
 
