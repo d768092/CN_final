89153ca51a79796dfbf32b9ebf605788b0e262d1
b06902067 <b06902067@linux2.csie.ntu.edu.tw>
2020-01-19 12:39:16 +0800

    delete file on getwhofile

--- checkfile.php
+++ checkfile.php
@@ -9,14 +9,10 @@ else{
 	exit;
 }
 	
-	//$path = $_POST['path'];
 	$jsonfile = 'upload_record.json';
-	//$filename = substr($path, 7);
 	$filename = $_POST['path'];
 	$namehash = hash('sha256', $username);
-	//echo substr($namehash,0,16)."\n";
 	$hash_filename = substr($namehash,0,16)."_".$filename;
-	//echo $hash_filename;
 	if(is_file($jsonfile)){
         $json_string = file_get_contents($jsonfile);
         $data = json_decode($json_string, true);
@@ -26,12 +22,10 @@ else{
 	$flag = "You can't access the file!";
 	if($filename=='') exit('');
 	foreach($data as $key => $value){
-		//echo $key." ".$hash_filename."\n";
         if(abs($time - $value[1]) > 300){
 			if($key == $hash_filename)	echo "File has been deleted by server!";
 			unset($data[$hash_filename]);
 			unlink("upload/".$hash_filename);
-			//echo "key is : ".$key." abs() is : ".abs($time-$value)."deleted".$hash_filename."\n";
 		}
 		else{
 			if($key == $hash_filename){
--- getwhofile.php
+++ getwhofile.php
@@ -12,14 +12,18 @@ else{
 	$jsonfile = 'upload_record.json';
 	$namehash = hash('sha256', $username);
 	if(is_file($jsonfile)){
-	$json_string = file_get_contents($jsonfile);
-	$data = json_decode($json_string, true);
-	$time = time();
-	}
+		$json_string = file_get_contents($jsonfile);
+		$data = json_decode($json_string, true);
+		$time = time();
 
-	foreach($data as $key => $value){
-		if(substr($key,0,16) == substr($namehash,0,16)){
-			echo $value[0]." send a file to you\n";
+		foreach($data as $key => $value){
+        	if(abs($time - $value[1]) > 300){
+				unset($data[$key]);
+				unlink("upload/".$key);
+			}
+			else if(substr($key,0,16) == substr($namehash,0,16)){
+				echo $value[0]." send a file to you\n";
+			}
 		}
 	}
 ?>
